b59fce801d06779cdaf7e41bce2c56f1e32c862b
Niklas Haag <niklas.haag@yahoo.de>
2023-05-18 20:21:40 +0200

    Add macbook_setup file

--- macbook_setup.sh
+++ macbook_setup.sh
@@ -8,6 +8,19 @@
 
 # Homebrew
 curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
-brew install tmux
-brew install neovim
-brew install fzf
+brew install tmux fzf yarn neovim mysql-client the_silver_searcher ctags
+brew install --HEAD universal-ctags/universal-ctags/universal-ctags
+brew tap microsoft/git
+brew install --cask git-credential-manager-core
+
+# nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+nvm install --lts
+nvm install lts/gallium
+nmv use lts/gallium
+nvm alias default lts/gallium
+
+npm install -g typescript
+### firebase login might only work with Chrome
+### => firebase login --no-localhost, copy url into chrome if chrome is not default
+npm install -g firebase-tools
