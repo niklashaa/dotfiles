1d53a83d7837acafe4ba45b466c69e633ed3e48f
Niklas Haag <niklas.haag@yahoo.de>
2023-05-18 07:59:57 +0200

    Add macbook_setup

---
+++ macbook_setup.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+
+# Software
+## Firefox
+## Chrome
+## KeepassXC
+## Spotify
+
+# Homebrew
+curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
+brew install tmux
+brew install neovim
+brew install fzf
