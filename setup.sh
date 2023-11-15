49e21ae72fe0537b215924c6e551c1d4abda6412
Niklas Haag <niklas.haag@yahoo.de>
2023-11-15 21:44:16 +0100

    Do symlink approach again

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+declare -a dotfiles=(".tmux.conf" ".zshrc" ".gitconfig" ".gitignore_global") #Declare dotfiles as array
+DOTFILEDIR=$HOME/.dotfiles
+CONFIGDIR=$HOME/.config
+
+for element in "${dotfiles[@]}"
+do
+    echo "Linking $DOTFILEDIR/${element} to $HOME/$element"
+    ln -sf "$DOTFILEDIR/${element}" "$HOME/$element"
+done
+
+echo "Linking $DOTFILEDIR/nvim to $CONFIGDIR/nvim"
+ln -sf "$DOTFILEDIR/nvim" "$CONFIGDIR/nvim"
