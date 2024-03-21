5db321f6408e19678a93298f09da1cc62278cbbb
Niklas Haag <niklas.haag@yahoo.de>
2024-03-21 18:25:30 +0100

    Move lualine into it's own file

--- nvim-kickstart/init.lua
+++ nvim-kickstart/init.lua
@@ -130,26 +130,6 @@ require('lazy').setup({
   },
 
   {
-    -- Set lualine as statusline
-    'nvim-lualine/lualine.nvim',
-    -- See `:help lualine.txt`
-    opts = {
-      options = {
-        icons_enabled = false,
-        -- theme = 'onedark',
-        theme = 'gruvbox',
-        component_separators = '|',
-        section_separators = '',
-      },
-      sections = {
-        lualine_c = {
-          { "filename", path = 1 },
-        },
-      }
-    },
-  },
-
-  {
     -- Add indentation guides even on blank lines
     'lukas-reineke/indent-blankline.nvim',
     -- Enable `lukas-reineke/indent-blankline.nvim`
