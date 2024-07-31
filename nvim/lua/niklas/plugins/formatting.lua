774752932df06609a7d6f8240568cb3c0194cafa
Niklas Haag <niklas.haag@yahoo.de>
2024-07-31 11:14:28 +0200

    formatting: Remove timeout_ms in favor of format_after_save

--- nvim/lua/niklas/plugins/formatting.lua
+++ nvim/lua/niklas/plugins/formatting.lua
@@ -3,7 +3,8 @@ return {
   opts = {
     notify_on_error = false,
     format_on_save = {
-      timeout_ms = 500,
+      format_after_save = true,
+      -- timeout_ms = 500,
       lsp_fallback = true,
     },
 
