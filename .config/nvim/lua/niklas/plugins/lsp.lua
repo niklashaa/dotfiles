69b88e82674feb0179a0d18c189464a21b2cedc5
Niklas Haag <niklas.haag@yahoo.de>
2025-06-01 22:41:12 +0200

    lsp: Remove warnings

--- .config/nvim/lua/niklas/plugins/lsp.lua
+++ .config/nvim/lua/niklas/plugins/lsp.lua
@@ -56,7 +56,7 @@ return {
       severity_sort = true,
       float = {
         border = 'rounded',
-        source = 'always',
+        source = true,
       },
     }
 
@@ -109,6 +109,8 @@ return {
     require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
     require('mason-lspconfig').setup {
+      ensure_installed = vim.tbl_keys(servers),
+      automatic_installation = true,
       handlers = {
         function(server_name)
           local server = servers[server_name] or {}
